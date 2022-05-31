efbb1fb261389a8fdacadccfac5657d62bd002e6
insung93 <jis5780@gmail.com>
2022-05-31 11:01:22 +0900

    logging, selectAll

---
+++ sts01/src/test/java/com/bit/model/Emp01DaoTest.java
@@ -0,0 +1,35 @@
+package com.bit.model;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.sql.SQLException;
+
+import org.apache.commons.logging.impl.Log4JLogger;
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Emp01DaoTest {
+	Logger log = Logger.getLogger(Emp01DaoTest.class);
+	Emp01Dao dao;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		dao = new Emp01Dao();
+	}
+
+	@Test
+	public void testSelectAll() throws ClassNotFoundException, SQLException {
+		assertTrue(dao.selectAll().size()>0);
+		for(EmpVo bean: dao.selectAll()) {
+			log.debug(bean);
+		}
+	}
+
+}
