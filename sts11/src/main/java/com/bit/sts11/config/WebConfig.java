799edda53a6c5408d0f440ad62e56cf142366a69
insung93 <jis5780@gmail.com>
2022-06-14 15:57:00 +0900

    java config

---
+++ sts11/src/main/java/com/bit/sts11/config/WebConfig.java
@@ -0,0 +1,54 @@
+package com.bit.sts11.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+//		root-context.xml
+//		<!-- The definition of the Root Spring Container shared by all Servlets and Filters -->
+//		<context-param>
+//			<param-name>contextConfigLocation</param-name>
+//			<param-value>/WEB-INF/spring/root-context.xml</param-value>
+//		</context-param>
+//		
+//		<!-- Creates the Spring Container shared by all Servlets and Filters -->
+//		<listener>
+//			<listener-class>org.springframework.web.context.ContextLoaderListener</listener-class>
+//		</listener>
+		
+		
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+//		servlet-context.xml
+//		<!-- Processes application requests -->
+//		<servlet>
+//			<servlet-name>appServlet</servlet-name>
+//			<servlet-class>org.springframework.web.servlet.DispatcherServlet</servlet-class>
+//			<init-param>
+//				<param-name>contextConfigLocation</param-name>
+//				<param-value>/WEB-INF/spring/appServlet/servlet-context.xml</param-value>
+//			</init-param>
+//			<load-on-startup>1</load-on-startup>
+//		</servlet>
+		
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+//		url mapping
+//		<servlet-mapping>
+//			<servlet-name>appServlet</servlet-name>
+//			<url-pattern>/</url-pattern>
+//		</servlet-mapping>
+		return new String[] {"/"};
+	}
+
+}
