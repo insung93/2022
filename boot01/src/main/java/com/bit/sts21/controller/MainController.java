3d4da83ba31d09f194f20ba8a3e9f3c0b005faff
insung93 <jis5780@gmail.com>
2022-06-15 17:54:25 +0900

    springboot start

---
+++ boot01/src/main/java/com/bit/sts21/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.bit.sts21.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	@GetMapping("/ex01")
+	public String welcome() {
+		return "ex01";
+	}
+}
